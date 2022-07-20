451b7e4c3b3a87f1c10dcdeaa5c8dc9d6a6863f6
yinianjiang <1737193419@qq.com>
2022-07-20 16:42:19 +0800

    pack

--- src/main/java/com/yinian/leetcode/design/Singleton4.java
+++ src/main/java/com/yinian/leetcode/design/Singleton4.java
@@ -2,10 +2,21 @@ package com.yinian.leetcode.design;
 
 public class Singleton4 {
     private static Singleton4 instance;
-    public static Singleton4 singleton4(){
+    private Singleton4(){
+        System.out.println(Thread.currentThread().getName()+"执行构造器");
+    }
+    public static Singleton4 getSingleton4(){
         if(instance == null){
             instance = new Singleton4();
         }
         return instance;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100 ; i++) {
+            new Thread(()->{
+                Singleton4.getSingleton4();
+            },String.valueOf(i)).start();
+        }
     }
 }
